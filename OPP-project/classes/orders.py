12983992b171ac5fdd2094005909f2d332ea45ce
Javier Pardos <pardosblesajavier@gmail.com>
2023-11-06 19:30:52 +0200

    just structure of orders file (implement in future commits)

---
+++ OPP-project/classes/orders.py
@@ -0,0 +1,29 @@
+"""
+Orders  -  models  an  order  to  be  placed  in  the  store.  Basically  a  collection  of  products  and  quantities  also 
+with a destination address
+"""
+
+from classes.product import Product
+
+class Orders(object):
+
+    orders = []
+
+    def __init__():
+        # TODO
+        pass
+
+    @classmethod
+    def add_order(cls, order):
+        # TODO
+        pass
+
+    @classmethod
+    def remove_order(cls, order):
+        # TODO
+        pass
+
+    @classmethod
+    def list_orders(cls):
+        # TODO
+        pass
