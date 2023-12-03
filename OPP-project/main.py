6721d2e8b0eb8752e67a840381b4dcd6bd0747a2
Javier Pardos <pardosblesajavier@gmail.com>
2023-12-03 12:31:00 +0200

    refactor main

--- OPP-project/main.py
+++ OPP-project/main.py
@@ -1,11 +1,17 @@
+# Advanced Python Programming - OPP Project | Author: Javier Pardos | javier.pardos10@e-uvt.ro
+
+from typing import List
+
 # impor from classes folder each file
 from classes.category import Category
 from classes.categories import Categories
+
+from classes.product import Product
 from classes.products import Products
-#from classes.product import Product
 from classes.amplifier import Amplifier
 from classes.receiver import Receiver
 from classes.turntable import Turntable
+
 from classes.order import Order
 from classes.orders import Orders
 
@@ -18,7 +24,8 @@ from test import test_main
 
 import logging
 #---------------------------------------------
-
+# LOGGING - Setup
+#---------------------------------------------
 def setupLogging(level: int = logging.INFO) -> None:
     # By default level is set to INFO, if no argument is passed to the function
     
@@ -31,7 +38,49 @@ def setupLogging(level: int = logging.INFO) -> None:
     logging.debug(f'setupLogging() ...')
 
 #---------------------------------------------
-# Logic functs
+# MENU UTILS - Logic functs 
+#---------------------------------------------
+
+def printNumberedMenu( list :list) -> None:
+    logging.debug(f'printNumberedMenu() ...')
+
+    # print all element on a numbered list
+
+    for i, element in enumerate(list):
+        print(f'\t{i}) {element.name}')
+
+
+def setProductForOrder(products :List[Product] ) -> Product:
+    while True:
+        try:
+            prodnum = int( input(f'\n\tType the number of the product to be ordered: ') )
+        except ValueError:
+            print('\n\t\t***INVALID INPUT: Please enter a valid number.')
+            continue
+        
+        # make sure user selected a product from the list
+        if 0 <= prodnum < len(products):
+            return products[prodnum]
+        elif prodnum == len(products):
+            return None
+        else:
+            print('\n\t\t***INVALID INPUT: Please enter a valid product number.')
+
+def setQuantityForOrder() -> int:
+    while True:
+        try:
+            quan = int(input(f'\n\tType how many of this product you want to order: '))
+            
+            # make sure quantity is greater than 0
+            if quan > 0:
+                return quan
+            else:
+                print(f'\n\t\t***INVALID INPUT: Quantity needs to be greater than 0, try again!')
+        except ValueError:
+            print('\n\t\t***INVALID INPUT: Please enter a valid number.')
+
+#---------------------------------------------
+# CATEGORY - Logic functs 
 #---------------------------------------------
 
 def addCategory():
@@ -47,14 +96,10 @@ def addCategory():
 def removeCategory():
     logging.debug(f'removeCategory() ...')
 
-    c = Categories.load_categories()
-    i = 0
-    print(f'\n')
-    for cat in c:
-        print(f'\t{i}) {cat.name}')
-        i += 1
+    printNumberedMenu(Categories.load_categories())
     
     catnum = int( input(f'\n\tType the number of the category to be removed: ') )
+
     cat = Categories.categories[catnum]
 
     if Categories.remove_category(cat):
@@ -62,78 +107,75 @@ def removeCategory():
     else:
         print(f'\t\tCategory not found!')
 
-
 def displayCategories():
     logging.debug(f'displayCategories() ...')
 
     Categories.list_categories()
 
 #---------------------------------------------
-
+# PRODUCT - Logic functs 
+#---------------------------------------------
 def addProduct():
     logging.debug(f'addProduct() ...')
 
     types_of_products = {1 : Amplifier, 2 : Receiver, 3 : Turntable}
     
+    # list the types of products
     for t in types_of_products:
         print(f'\t{t}) {types_of_products[t].__name__}')
     
     p = int(input(f'\n\tType the number of the product to: '))
     
+    # check if the product type exists
     if p not in types_of_products:
         print(f'\t\tProduct not found!')
         return
+
+    #Ask for details of the product
+    info = {"name" : "", "price" : 0}
+    product = None
+
+    # Ask for comon info of the product
+    info["name"] = input(f'\t\tType the name of the new {types_of_products[p].__name__}: ')
+    info["price"] = float(input(f'\t\tType the price of the new {types_of_products[p].__name__}: '))
+
+    # Ask for specific info of the product
+    if types_of_products[p].__name__ == Amplifier.__name__:
+
+        info["power"] = int(input(f'\t\tType the power of the new {types_of_products[p].__name__}: '))
+        info["num_channels"] = int(input(f'\t\tType the number of channels of the new {types_of_products[p].__name__}: '))
+        info["size"] = input(f'\t\tType the size of the new {types_of_products[p].__name__}: ')
+
+    elif types_of_products[p].__name__ == Receiver.__name__:
+
+        info["num_channels"] = int(input(f'\t\tType the number of channels of the new {types_of_products[p].__name__}: '))
+        info["color"] = input(f'\t\tType the color of the new {types_of_products[p].__name__}: ')
+        info["size"] = input(f'\t\tType the size of the new {types_of_products[p].__name__}: ')
+
+    elif types_of_products[p].__name__ == Turntable.__name__:
+
+        info["speed"] = float(input(f'\t\tType the speed of the new {types_of_products[p].__name__}: '))
+        info["connection_type"] = input(f'\t\tType the connection type (wired or bluetooth) of the new {types_of_products[p].__name__}: ')
+        info["size"] = input(f'\t\tType the size of the new {types_of_products[p].__name__}: ')
+
+    #Create the product
+    product = types_of_products[p](**info)
+
+    # Add the product to the list of products
+    if Products.add_product(product):
+        print(f'\n\t\tProduct added successfully!')
     else:
+        print(f'\n\t\tProduct already exists!')
         
-        #Ask for details of the product
-        info = {"name" : "", "price" : 0}
-        product = None
-
-        # Ask for comon info of the product
-        info["name"] = input(f'\t\tType the name of the new {types_of_products[p].__name__}: ')
-        info["price"] = float(input(f'\t\tType the price of the new {types_of_products[p].__name__}: '))
-
-        # Ask for specific info of the product
-        if types_of_products[p].__name__ == Amplifier.__name__:
-
-            info["power"] = int(input(f'\t\tType the power of the new {types_of_products[p].__name__}: '))
-            info["num_channels"] = int(input(f'\t\tType the number of channels of the new {types_of_products[p].__name__}: '))
-            info["size"] = input(f'\t\tType the size of the new {types_of_products[p].__name__}: ')
-
-        elif types_of_products[p].__name__ == Receiver.__name__:
-
-            info["num_channels"] = int(input(f'\t\tType the number of channels of the new {types_of_products[p].__name__}: '))
-            info["color"] = input(f'\t\tType the color of the new {types_of_products[p].__name__}: ')
-            info["size"] = input(f'\t\tType the size of the new {types_of_products[p].__name__}: ')
-
-        elif types_of_products[p].__name__ == Turntable.__name__:
-
-            info["speed"] = float(input(f'\t\tType the speed of the new {types_of_products[p].__name__}: '))
-            info["connection_type"] = input(f'\t\tType the connection type (wired or bluetooth) of the new {types_of_products[p].__name__}: ')
-            info["size"] = input(f'\t\tType the size of the new {types_of_products[p].__name__}: ')
-
-        #Create the product
-        product = types_of_products[p](**info)
-
-        # Add the product to the list of products
-        if Products.add_product(product):
-            print(f'\n\t\tProduct added successfully!')
-        else:
-            print(f'\n\t\tProduct already exists!')
-        
-        #TODO: not working properly: add_product() returns always True ??? (check it)
+    #TODO: not working properly: add_product() returns always True ??? (check it)
 
 def removeProduct():
     logging.debug(f'removeProduct() ...')
 
-    p = Products.load_products()
-    i = 0
-    print(f'\n')
-    for pro in p:
-        print(f'\t{i}) {pro.get_details()}')
-        i += 1
+    printNumberedMenu(Products.load_products())
+
+    pronum = int( input(f'\n\tType the number of the product to be removed: ') )
     
-    pronum = input(f'\n\tType the number of the product to be removed: ')
     pro = Products.products[int(pronum)]
 
     if Products.remove_product(pro):
@@ -141,87 +183,56 @@ def removeProduct():
     else:
         print(f'\t\tProduct not found!')
 
-
 def displayProducts():
     logging.debug(f'displayProducts() ...')
 
     Products.list_products()
 
-
 #---------------------------------------------
-
+# ORDER - Logic functs 
+#---------------------------------------------
 def placeOrder():
     logging.debug(f'placeOrder() ...')
 
-    p = Products.load_products()
-    
-    if len(p) == 0: 
+    products = Products.load_products()
+
+    # make sure the shop has products to order
+    if len(products) == 0: 
         print(f'\tAdd some products to the order catalog!')
         return
 
-    # set the number for the option to exit the menu for adding products to the order
-    exitnum = len(p)
-    
-    # set up the vars for the order object constructor
-    productsorder = []
-    quantitiesorder = []
-    address = ""
-    
-    # fill the address
     address = input(f'\n\tType your address: ')
-
     print(f'\n')
 
     # print all products avaliable in the shop
-    for i, pro in enumerate(p):
+    for i, pro in enumerate(products):
         print(f'\t{i}) {pro.get_details()}')
+    
+    # option to exit the loop of adding products
+    print(f'\t{len(products)}) --- End Order ---')
 
-    print(f'\t{exitnum}) --- End Order ---')
+    # set up the vars for the order object constructor
+    productsorder = []
+    quantitiesorder = []
 
     # loop to add more than one product
     while True:
-
-        # error handlelinng for product selection
-        try:
-            prodnum = int(input(f'\n\tType the number of the product to be ordered: '))
-        except ValueError:
-            print('\n\t\t***INVALID INPUT: Please enter a valid number.')
-            continue
+        product = setProductForOrder(products)
         
-        # if user want to exit 
-        if prodnum == exitnum:
-            # exit while loop
+        # if user want to exit, product is None
+        if product is None:
             break
-        # make sure user selected a product 
-        elif 0 <= prodnum < len(p):
-            
-            # set up a var outside de loop to retrieve user choice
-            quan = 0
-            
-            # loop to select quantities for this product
-            while True:
-                try:
-                    quan = int(input(f'\n\tType how many of this product you want to order: '))
-                    
-                    if quan > 0:
-                        break
-                    else:
-                        print(f'\n\t\t***INVALID INPUT: Quantity needs to be greater than 0, try again!')
-                except ValueError:
-                    print('\n\t\t***INVALID INPUT: Please enter a valid number.')
 
-            # append products and quantities to list
-            productsorder.append(p[prodnum])
-            quantitiesorder.append(quan)
+        quantity = setQuantityForOrder()
+
+        productsorder.append(product)
+        quantitiesorder.append(quantity)
         
-        else:
-            print('\n\t\t***INVALID INPUT: Please enter a valid product number.')
-
     # create order with the parameters the user filled in the step by step before
-    o = Order(productsorder, quantitiesorder, address)
+    order = Order(productsorder, quantitiesorder, address)
 
     # check return code of the process of adding a new order 
-    if Orders.add_order(o):
+    if Orders.add_order(order):
         print(f'\t\tOrder added successfully')
     else:
         print(f'\t\tThere was an error while processing your order! ...')
@@ -230,7 +241,11 @@ def displayOrders():
     logging.debug(f'displayOrders() ...')
 
     Orders.list_orders()
-    
+
+#---------------------------------------------
+# EXIT - Logic functs 
+#---------------------------------------------
+
 def exit_loop():
     logging.debug(f'exit_loop() ...')
     exit(0)
@@ -272,7 +287,6 @@ def main():
     
     while True:
         
-
         for menu in menuList:
             print(menu)
         
@@ -281,7 +295,6 @@ def main():
         op = int( input("\nEnter your operation:") )
         func = operations.get(op, error_handler)
         
-
         func()
 
         print(f'\n{"*" * 50}\n')
