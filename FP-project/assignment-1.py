db6d5cfc055c287bf506dd9b0c181df20ec5729d
Javier <pardosblesajavier@gmail.com>
2023-12-22 13:23:48 +0100

    done fp project

--- FP-project/assignment-1.py
+++ FP-project/assignment-1.py
@@ -1,1 +1,69 @@
 # Advanced Python Programming - FP Project | Author: Javier Pardos | javier.pardos10@e-uvt.ro
+
+"""
+Assigment 1 - Write a program that reads a text file, decodes it with the given replacements
+and prints on the screen all the words starting with 'a'.
+
+All these operations will be implemented using the functional programming style
+"""
+
+from typing import List
+
+def replace_char(char :chr) -> chr:
+    """ Pure function - Replaces a character with another one """
+
+    replacements = {'!':'s', '@':'h', '#':'e', '$':'r', '%':'l', '^':'o', '&':'c', '*':'k'}
+
+    # return the replacement key if exists in dict, else return the same char that was given 
+    # becasuse it means that it is not a special character
+    return replacements.get(char, char)
+
+
+def decode_text(text: str) -> str:
+
+    """ Decodes the text with the given replacements """
+
+    # 'join()' doesnt modifies the original string, it returns a new one
+    # 'map()'  is given an iterable collection, and a function that it will apply to each element 
+    #          in this case the string its the original text and 'replace_char()' is the function 
+    #          that will be applied to each char
+
+    return ''.join(map(replace_char, text))
+
+
+def find_words(text: str, letter: str) -> List[str]:
+    
+    # split the text by spaces, into a list of words
+    splitted_text = text.split()
+
+    
+    # 'filter()'  is given an iterable collection, splitted_text (words from text)
+    #             and a function that will be applied to each element, 'startswith()'
+    #
+    # lambda function is used to iterate over the list of words and check 
+    #                 if the word starts with the given letter, as an anonymous function
+    #
+    # return a list with the result of the filter function
+    return list( filter(lambda word: word.startswith(letter), splitted_text) )
+
+def main():
+
+    # read the file sherlock.txt
+    with open('sherlock.txt', 'r') as file:
+        text = file.read()
+
+    ##print(text)
+
+    # apply the decode function to the text
+    decoded_text = decode_text(text)
+
+    ##print(decoded_text)
+
+    # find words starting with 'a' 
+    words = find_words(decoded_text, 'a')
+
+    # print the words
+    print(words)
+
+if __name__ == '__main__':
+    main()
--- FP-project/assignment-3.py
+++ FP-project/assignment-3.py
@@ -1,1 +1,142 @@
 # Advanced Python Programming - FP Project | Author: Javier Pardos | javier.pardos10@e-uvt.ro
+
+from typing import List, Tuple
+
+#---------------------------------------------
+# DEBUG
+#---------------------------------------------
+import logging
+
+"""
+Assigment 3 - Create a program that manages a list of movies and implement the following functionality:
+
+1. A movie is defined by a title and a rank (like the one defined in imdb.com)
+2. Create the possibility to add a movie in the list of movies
+3. Create the possibility to display all the movies existing at a certain moment in time
+4. Implement the possibility to search the movies for a specific movie
+5. Create the possibility of removing of a certain movie from the movies list
+
+"""
+#---------------------------------------------
+# LOGGING - Setup
+#---------------------------------------------
+def setupLogging(level: int = logging.INFO) -> None:
+    # By default level is set to INFO, if no argument is passed to the function
+    
+    # Log level: ----------------------------------------------------
+    fmt = '[%(levelname)s : %(asctime)s - %(message)s]'
+    # Adjust log level, format msg and date format
+    logging.basicConfig(level=level, format=fmt, datefmt='%d-%m-%Y %H:%M:%S')
+    # Log level: ----------------------------------------------------
+
+    logging.debug(f'setupLogging() ...')
+
+
+def addMovie(movies: List[Tuple[str, float]]) -> List[Tuple[str, float]]:
+    
+    """ Pure function () - Adds a movie to the list of movies """
+    
+    logging.debug(f'addMovie()...')
+
+    title = input(f'\tEnter the title of the movie: ')
+    rank = float(input(f'\tEnter the rank of the movie: '))
+
+    # return original list + new movie
+    return movies + [(title, rank)]
+
+def displayMovies(movies :List[ Tuple[str, float] ]) -> None:
+    
+    """ Pure function () - Displays all the movies existing at a certain moment in time """
+    
+    logging.debug(f'displayMovies()...')
+
+    for title, rank in movies:
+        print(f'\tTittle: {title} - Ranking: {rank}')
+
+def findMovie(movies :List[ Tuple[str, float] ]) -> None:
+
+    """ Pure function () - Search the movies for a specific movie """
+
+    logging.debug(f'findMovie()...')
+
+    title = input('\tEnter the title of the movie: ')
+
+    # lambda function to check if title given is the same from any movie 
+    #
+    # 'filter()' iterates over the list of movies and applies 
+    #            the lambda function to each element
+    movie = list(filter(lambda m: m[0] == title, movies))
+
+    # print the movie if exists, else print 'Movie not found'
+    if movie: print(f'\t{movie[0][0]} -  {movie[0][1]}')
+    else: print(f'\tMovie not found')
+
+def removeMovie(movies :List[ Tuple[str, float] ]) -> List[ Tuple[str, float] ]:
+    
+    """ Removes a movie from the list of movies """
+    
+    logging.debug(f'removeMovie()...')
+    
+    title = input(f'\tEnter the title of the movie: ')
+    
+    # lambda function to check if title given is not is the same from any movie
+    #
+    # 'filter()' iterates over the list of movies and applies the filter 
+    #            function to each element 
+    return list(filter(lambda m: m[0] != title, movies))
+
+def exit_loop():
+    logging.debug(f'exit_loop()...')
+    exit(0)
+
+def error_handler():
+    print("This operation does not exist ...")
+
+
+def main():
+
+    # create an empty dict for the movies: tittle -> rating 
+    movies = []
+
+
+    menuList = [
+        '1. Add a movie',
+        '2. Display all movies',
+        '3. Search for a movie',
+        '4. Remove a movie',
+        '5. Exit'
+    ]
+
+    operations = { 1 : addMovie, 2 : displayMovies, 
+                   3 : findMovie, 4 : removeMovie,
+                   5 : exit_loop}
+
+    #Header 
+    print(f'\n{"*" * 50}\n')
+
+    while True:
+        
+        for menu in menuList:
+            print(menu)
+        
+        print(f'\n{"*" * 50}\n')
+        
+        op = int( input("\nEnter your operation:") )
+        func = operations.get(op, error_handler)
+        
+        # if op is 5, exit the program
+        if op == 5:
+            func()
+        else:
+            updated_list = func(movies)
+            
+            # if the function returns a list, update the movies list
+            if  updated_list is not None:
+                movies = updated_list
+        
+        print(f'\n{"*" * 50}\n')
+        #Clear de console
+        
+
+if __name__ == '__main__':
+    main()
